84b36b706b16d3b0ab8265cae3ec9674e1822f87
usuario <usuario@PC>
2017-02-05 21:06:37 +0100

    insert added

--- FacturerPro/FacturerPro/Form1.cs
+++ FacturerPro/FacturerPro/Form1.cs
@@ -10,11 +10,17 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
 namespace FacturerPro
 {
     public partial class Form1 : Form
     {
+
+        
+
+        
         public Form1()
         {
             InitializeComponent();
@@ -43,7 +49,7 @@ namespace FacturerPro
 
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Console.WriteLine("Hola");
+           // Console.WriteLine("Hola");
 
             switch (tabControl1.SelectedIndex)
             {
@@ -81,7 +87,7 @@ namespace FacturerPro
         private void button2_Click(object sender, EventArgs e)//Importar
         {
             DataSet dataSet = this.pcgroundDataSet;
-            DataTableHelper.ReadXmlIntoDataSet(dataSet, "new.xml");
+            DataTableHelper.ReadXmlIntoDataSet(dataSet, "old.xml");
         }
     }
 
@@ -114,47 +120,132 @@ namespace FacturerPro
             XmlDocument doc = new XmlDocument();
             doc.Load(xmlFileName);
             XmlElement root = doc.DocumentElement;
-            XmlNodeList nodes = root.SelectNodes("/descendant-or-self::node()"); 
+            XmlNodeList nodes = root.SelectNodes("/descendant-or-self::node()");
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+            myConnectionString = "Database = pcground; Password = root; Port = 3307; Server = localhost; User = root";
+            
+            //List<string> parts = new List<string>();
             foreach (XmlNode node in nodes)
             {
                 switch (node.Name) {
 
+                    case "clients":
+                        XmlNodeList childones03 = node.ChildNodes;
+                        List<string> clients = new List<string>();
+                        foreach (XmlNode node01 in childones03)
+                        {
+                            //Console.WriteLine(node01.Name + " -> " + node01.InnerText);
+                            clients.Add(node01.InnerText);
+                        }
+                        try
+                        {
+                            conn = new MySql.Data.MySqlClient.MySqlConnection();
+                            conn.ConnectionString = myConnectionString;
+                            MySqlCommand command = conn.CreateCommand();
+                            command.CommandText = "INSERT INTO clients (id_Client,Nom,Cognom1,Cognom2,Adreça,Codi_Postal,Població,Província,Telèfon,Fax,E-mail) VALUES (?id_Client,?Nom,?Cognom1,?Cognom2,?Adreça,?Codi_Postal,?Població,?Província,?Telèfon,?Fax,?E-mail)";
+                            command.Parameters.AddWithValue("?id_Client", clients[0]);
+                            command.Parameters.AddWithValue("?Nom", clients[1]);
+                            command.Parameters.AddWithValue("?Cognom1", clients[2]);
+                            command.Parameters.AddWithValue("?Cognom2", clients[3]);
+                            command.Parameters.AddWithValue("?Adreça", clients[4]);
+                            command.Parameters.AddWithValue("?Codi_Postal", clients[5]);
+                            command.Parameters.AddWithValue("?Població", clients[6]);
+                            command.Parameters.AddWithValue("?Província", clients[7]);
+                            command.Parameters.AddWithValue("?Telèfon", clients[8]);
+                            command.Parameters.AddWithValue("?Fax", clients[9]);
+                            command.Parameters.AddWithValue("?E-mail", clients[10]);
+                            conn.Open();
+                            command.ExecuteNonQuery();
+                        }
+                        catch (MySql.Data.MySqlClient.MySqlException ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                        Console.WriteLine();
+                    break;
+
                     case "factura":
                         XmlNodeList childones01 = node.ChildNodes;
-
+                        List<string> factures = new List<string>();
                         foreach (XmlNode node01 in childones01)
                         {
-                            Console.WriteLine(node01.Name + " -> " + node01.InnerText);
+                            //Console.WriteLine(node01.Name + " -> " + node01.InnerText);
+                            factures.Add(node01.InnerText);    
+                        }
+                        try
+                        {
+                            conn = new MySql.Data.MySqlClient.MySqlConnection();
+                            conn.ConnectionString = myConnectionString;
+                            MySqlCommand command = conn.CreateCommand();
+                            command.CommandText = "INSERT INTO factura (n_Factura,id_Client,Data,Descompte,IVA) VALUES (?n_Factura,?id_Client,?Data,?Descompte,?IVA)";
+                            command.Parameters.AddWithValue("?n_Factura", factures[0]);
+                            command.Parameters.AddWithValue("?id_Client", factures[1]);
+                            command.Parameters.AddWithValue("?Data", factures[2]);
+                            command.Parameters.AddWithValue("?Descompte", factures[3]);
+                            command.Parameters.AddWithValue("?IVA", factures[4]);
+                            conn.Open();
+                            command.ExecuteNonQuery();
+                        }
+                        catch (MySql.Data.MySqlClient.MySqlException ex)
+                        {
+                            MessageBox.Show(ex.Message);
                         }
                         Console.WriteLine();
                         break;
 
                     case "productes":
                         XmlNodeList childones02 = node.ChildNodes;
-
+                        List<string> productes = new List<string>();
                         foreach (XmlNode node01 in childones02)
                         {
-                            Console.WriteLine(node01.Name + " -> " + node01.InnerText);
+                            //Console.WriteLine(node01.Name + " -> " + node01.InnerText);
+                            productes.Add(node01.InnerText);
                         }
-                        Console.WriteLine();
-                        break;
-
-                    case "clients":
-                        XmlNodeList childones03 = node.ChildNodes;
-
-                        foreach (XmlNode node01 in childones03)
+                        try
                         {
-                            Console.WriteLine(node01.Name + " -> " + node01.InnerText);
+                            conn = new MySql.Data.MySqlClient.MySqlConnection();
+                            conn.ConnectionString = myConnectionString;
+                            MySqlCommand command = conn.CreateCommand();
+                            command.CommandText = "INSERT INTO productes (id_Producte,Producte,Preu) VALUES (?id,?Producte,?Preu)";
+                            command.Parameters.AddWithValue("?id", productes[0]);
+                            command.Parameters.AddWithValue("?Producte", productes[1]);
+                            command.Parameters.AddWithValue("?Preu", productes[2]);
+                            conn.Open();
+                            command.ExecuteNonQuery();
+                        }
+                        catch (MySql.Data.MySqlClient.MySqlException ex)
+                        {
+                            MessageBox.Show(ex.Message);
                         }
                         Console.WriteLine();
                         break;
+
+                    
 
                     case "factura_detall":
                         XmlNodeList childones04 = node.ChildNodes;
-
+                        List<string> facturesDetall = new List<string>();
                         foreach (XmlNode node01 in childones04)
                         {
-                            Console.WriteLine(node01.Name + " -> " + node01.InnerText);
+                            //Console.WriteLine(node01.Name + " -> " + node01.InnerText);
+                            facturesDetall.Add(node01.InnerText);
+                        }
+                        try
+                        {
+                            conn = new MySql.Data.MySqlClient.MySqlConnection();
+                            conn.ConnectionString = myConnectionString;
+                            MySqlCommand command = conn.CreateCommand();
+                            command.CommandText = "INSERT INTO factura_detall (n_Factura,id_Producte,Quantitat) VALUES (?n_Factura,?id_Producte,?Quantitat)";
+                            command.Parameters.AddWithValue("?n_Factura", facturesDetall[0]);
+                            command.Parameters.AddWithValue("?id_Producte", facturesDetall[1]);
+                            command.Parameters.AddWithValue("?Quantitat", facturesDetall[2]);
+                            conn.Open();
+                            command.ExecuteNonQuery();
+                        }
+                        catch (MySql.Data.MySqlClient.MySqlException ex)
+                        {
+                            MessageBox.Show(ex.Message);
                         }
                         Console.WriteLine();
                         break;
@@ -220,6 +311,31 @@ namespace FacturerPro
                 }
                 Console.WriteLine("{0,-14}", "");
             }
+        }
+
+
+        public static void UpdateBindingNavigator() {
+            SqlDataAdapter datapter;
+            DataSet dset;
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+            myConnectionString = "Database = pcground; Password = root; Port = 3307; Server = localhost; User = root";
+            conn = new MySql.Data.MySqlClient.MySqlConnection();
+            conn.ConnectionString = myConnectionString;
+            datapter = new SqlDataAdapter("SELECT * FROM clients", myConnectionString);
+            dset = new DataSet();
+            datapter.Fill(dset);
+
+            //BindingSource1.DataSource = dset.Tables[0];
+
+
+
+        }
+
+        public static void ClearBeforeFill()
+        {
+            //Netajem els bindings Sources
+            //txtbox1.DataBindings.Clear();
         }
     }
 }
